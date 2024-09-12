d440f67fb8cf7aec006b0d88d03a4736db723e52
abl <ablay.yedilbayev@gmail.com>
2024-09-12 09:10:38 +0500

    Fields injection

---
+++ app/src/main/java/com/example/dependencyinjectionstart/example1/Component.kt
@@ -0,0 +1,22 @@
+package com.example.dependencyinjectionstart.example1
+
+class Component {
+
+    private fun getComputer(): Computer {
+        val monitor = Monitor()
+        val computerTower = ComputerTower(
+            storage = Storage(),
+            memory = Memory(),
+            processor = Processor()
+        )
+        val keyboard = Keyboard()
+        val mouse = Mouse()
+        return Computer(monitor, computerTower, keyboard, mouse)
+    }
+
+    fun inject(activity: Activity) {
+        activity.computer = getComputer()
+        activity.keyboard = Keyboard()
+    }
+
+}
