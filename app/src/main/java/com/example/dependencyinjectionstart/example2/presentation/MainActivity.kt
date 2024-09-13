17c564c5c6a7baeff0f5b7a7e73c2489a30746f4
abl <ablay.yedilbayev@gmail.com>
2024-09-13 09:02:22 +0500

    Prepare example2

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
@@ -2,23 +2,16 @@ package com.example.dependencyinjectionstart.example2.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.dependencyinjectionstart.R
 import com.example.dependencyinjectionstart.example1.Activity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: ExampleViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        val activity = Activity()
-        activity.computer.toString()
+        viewModel.method()
     }
 }
