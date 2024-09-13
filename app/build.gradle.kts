665e31767fbca3cc335309af06732b642f66cddc
abl <ablay.yedilbayev@gmail.com>
2024-09-13 08:15:29 +0500

    Add Dagger2 dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    kotlin("kapt") // Add this line
 }
 
 android {
@@ -45,4 +46,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Dagger core library
+    implementation(libs.dagger)
+
+    // Dagger annotation processor
+    kapt(libs.dagger.compiler)
 }
